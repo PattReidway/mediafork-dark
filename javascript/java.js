76bc7a90c69a846999dbc7fb9b82be328020f1ad
benjamin ebran <ebran.b@gmail.com>
2022-06-16 16:53:55 +0200

    transition

--- javascript/java.js
+++ javascript/java.js
@@ -1,5 +1,15 @@
-const burgerButton = document.getElementsByClassName("fa-bars")
 
-burgerButton.addEventListener("click" () {
-    console.log(burgerButton)
+const burgerButton = document.querySelector("nav .nav-burger");
+const faBars = document.querySelector(".fa-bars")
+burgerButton.addEventListener("click" ,function(event){
+   
+    document.querySelector("nav .nav-list").classList.toggle("active")
+    document.querySelector(".fa-bars").classList.toggle("fa-angle-up");
+
+
+       if(faBars.classList.contains("fa-angle-up")){
+        document.documentElement.style.overflow = 'hidden';
+   }else{
+    document.documentElement.style.overflow = 'scroll';
+   }
 })
